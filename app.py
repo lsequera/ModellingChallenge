edfcc7ef6dd909bc30c9d713449c927d241a2070
Ling <lsequera@proton.me>
2023-02-10 11:55:58 -0500

    saving model status acc=0.7

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import pickle
+
+model = pickle.load(open("churn/models/model.pk", "rb"))
+
+
 app = FastAPI()
 
 # class Data(BaseModel):
